5ccff2eb6e38abaad9fbe5d151620ea870de7f20
pstancato <paolo.stancato@fluxit.com.ar>
2017-02-12 12:22:01 -0300

    Refactored window.search() to predict in batches

--- p5lib/window.py
+++ p5lib/window.py
@@ -55,24 +55,32 @@ def search(img, windows, clf, color_space='RGB', threshold=0.5,
                     orient=9, pix_per_cell=8, cell_per_block=2, hog_channel=0,
                     spatial_feat=True, hist_feat=True, hog_feat=True):
 
-    #1) Create an empty list to receive positive detection windows
-    on_windows = []
-    windows_prob = []
-    #2) Iterate over all windows in the list
+
+    #1) Create an empty list to append the features for all images
+    image_set = []
+    features_set = []
+    #2) Iterate over all windows in the list and extract features
     for window in windows:
         #3) Extract the test window from original image
-        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]], (64, 64))      
-        #4) Extract features for that window using single_img_features()
-        features = feature_extraction.all_features([test_img], color_space=color_space, 
-                            spatial_size=spatial_size, hist_bins=hist_bins, 
-                            orient=orient, pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, hog_channel=hog_channel,
-                            spatial_feat=spatial_feat, hist_feat=hist_feat, hog_feat=hog_feat)
-        #6) Predict using your classifier
-        prediction = clf.predict_proba(features)
+        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]], (64, 64))
+        image_set.append(test_img)
+    #4) Extract features for all windows
+    features = feature_extraction.all_features(image_set, color_space=color_space, 
+                        spatial_size=spatial_size, hist_bins=hist_bins, 
+                        orient=orient, pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, hog_channel=hog_channel,
+                        spatial_feat=spatial_feat, hist_feat=hist_feat, hog_feat=hog_feat)
+    #6) Predict using the classifier for all windows
+    predictions = clf.predict_proba(features)
+
+    # Create an empty list to receive positive detection windows
+    on_windows = []
+    windows_prob = []
+    # Iterate over all windows and its probabilities
+    for prediction, window in zip(predictions, windows):
         #7) If probability is above the threshold, then save the window
-        if prediction[0][1] > threshold:
+        if prediction[1] > threshold:
             on_windows.append(window)
-            windows_prob.append(prediction[0][1])
+            windows_prob.append(prediction[1])
 
     #8) Return windows for positive detections
     return on_windows, windows_prob
